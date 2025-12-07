287149aa9d46289b601bafc668bde82a7a800787
Shibil-Basith <shibilbasith4u@gmail.com>
2025-12-07 20:55:23 +0530

    Fix empty env

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
 exports.connectDB = () => {
-    mongoose.connect(process.env.DATABASE_URL, {
+    // CHANGED: specifically use MONGODB_URL to match the OpenShift/Docker config
+    mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-        .then(() => {
-            console.log('Database connected succcessfully');
-        })
-        .catch(error => {
-            console.log(`Error while connecting server with Database`);
-            console.log(error);
-            process.exit(1);
-        })
+    .then(() => {
+        console.log('✅ Database connected successfully');
+    })
+    .catch(error => {
+        console.log(`❌ Error while connecting server with Database`);
+        console.log(error);
+        process.exit(1);
+    })
 };
-
